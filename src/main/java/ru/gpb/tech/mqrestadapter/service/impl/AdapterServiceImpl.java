dcdc04bf7a245a3c1d7eac4055cb24e3df28d565
Савкин Алексей <Aleksey.Savkin@gazprombank.ru>
2024-07-18 20:37:17 +0300

    Реализация шаблона отказоустойчивости Circuit Breaker

--- src/main/java/ru/gpb/tech/mqrestadapter/service/impl/AdapterServiceImpl.java
+++ src/main/java/ru/gpb/tech/mqrestadapter/service/impl/AdapterServiceImpl.java
@@ -6,12 +6,12 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import ru.gpb.tech.mqrestadapter.config.FeatureToggleConfig;
-import ru.gpb.tech.mqrestadapter.feign.WalletClient;
 import ru.gpb.tech.mqrestadapter.mapper.WalletBalanceMapper;
 import ru.gpb.tech.mqrestadapter.model.ClientRequest;
 import ru.gpb.tech.mqrestadapter.model.ClientResponse;
 import ru.gpb.tech.mqrestadapter.service.AdapterService;
 import ru.gpb.tech.mqrestadapter.service.MqPublisher;
+import ru.gpb.tech.mqrestadapter.service.WalletService;
 
 import java.math.BigDecimal;
 import java.util.Optional;
@@ -23,11 +23,11 @@ import java.util.Optional;
 @Service
 @RequiredArgsConstructor
 public class AdapterServiceImpl implements AdapterService {
-    private final WalletClient walletClient;
+    private final WalletService walletService;
     private final WalletBalanceMapper mapper;
     private final MqPublisher publisher;
     private final FeatureToggleConfig featureToggleConfig;
-    
+
     /**
      * Обрабатывает запрос клиента, создает ответ клиента и отправляет его через {@link MqPublisher}.
      * Если интеграция с кошельком включена, используется новая логика, иначе используется старая логика.
@@ -44,24 +44,22 @@ public class AdapterServiceImpl implements AdapterService {
             executeOldLogic(request);
         }
     }
-    
+
     private void executeOldLogic(ClientRequest clientRequest) {
         log.info("Выполнение старой логики сервиса");
         ClientResponse clientResponse = new ClientResponse();
         clientResponse.setLastUpdated(clientRequest.getDateTo());
         clientResponse.setBalance(BigDecimal.ONE);
         clientResponse.setCurrency("RUB");
-        
+
         publisher.sendSuccess(clientResponse);
     }
-    
+
     private void executeNewLogic(ClientRequest request) {
         log.info("Выполнение новой логики сервиса");
         Optional.ofNullable(request)
                 .map(mapper::toWalletRequest)
-                .map(walletClient::getBalance)
-                .filter(response -> HttpStatus.OK.equals(response.getStatusCode()))
-                .map(HttpEntity::getBody)
+                .flatMap(walletService::getBalance)
                 .map(mapper::toClientResponse)
                 .ifPresentOrElse(publisher::sendSuccess,
                         () -> publisher.sendError("При обработке запроса произошла ошибка"));
